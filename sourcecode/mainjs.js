b17b62ff7b2ea6434bb7687d80c4a5d0babb7c8b
kahjee <kagieangelomaruyama@gmail.com>
2022-01-04 16:00:39 +0800

    Added JS basis and input for website.

--- sourcecode/mainjs.js
+++ sourcecode/mainjs.js
@@ -1,3 +1,13 @@
 function printer(){
     console.log("Console test. If this function shows in console, this means the javascript functions will work.");
 }
+
+const age = document.getElementById('age');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const activity = document.getElementById('list').value;
+const calories = document.getElementById('calories');
+
+function caloriecalc(){
+    
+}
