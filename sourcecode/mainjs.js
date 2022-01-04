a0b2cf616b26f7bc05cf2ac92ed604e0684f4e0a
kahjee <kagieangelomaruyama@gmail.com>
2022-01-04 19:32:23 +0800

    Adjustment to JS file.

--- sourcecode/mainjs.js
+++ sourcecode/mainjs.js
@@ -2,12 +2,23 @@ function printer(){
     console.log("Console test. If this function shows in console, this means the javascript functions will work.");
 }
 
+printer();
+
 const age = document.getElementById('age');
 const height = document.getElementById('height');
 const weight = document.getElementById('weight');
-const activity = document.getElementById('list').value;
+const activity = document.getElementById('list');
 const calories = document.getElementById('calories');
 
-function caloriecalc(){
-    
+function noInput(){
+    alert("Please make sure that you have inputted all of the values, otherwise the program would not work");
+    errorMessage("Error, no input");
+}
+
+if (age || ""){
+    noInput();
+}
+
+else if (age){
+
 }
